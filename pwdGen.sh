d7e8ce8a54ec306f6ec3d7489e25531ada754a49
Azukuni <63687010+Asriel115@users.noreply.github.com>
2025-05-14 17:52:08 -0400

    Update pwdGen.sh
    I made this better and more usable

--- pwdGen.sh
+++ pwdGen.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
-# Define the length of the password
-length=40
+
+# Ask user for password length
+read -r -p "How long should the password be? (default: 40): " length
+length=${length:-40} # Set default to 40 if empty
+
+# Validate input is a positive number
+while ! [[ "$length" =~ ^[0-9]+$ ]] || [ "$length" -le 0 ]; do
+    read -r -p "Please enter a valid positive number: " length
+done
 
 # Define the possible characters for the password
 chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()~,.|-+=/"
 
 while true; do
-  # Generate the password
-  password=""
-  for ((i=0; i<length; i++)); do
-    password+=${chars:$(( RANDOM % ${#chars} )):1}
-  done
+    # Generate the password
+    password=""
+    for ((i = 0; i < length; i++)); do
+        password+=${chars:$((RANDOM % ${#chars})):1}
+    done
 
-  # Print the password and ask if it should be saved
-  echo "Generated Password: $password"
-  read -r -p "Do you want to save this password? (y/n): " response
-  case $response in
-    [Yy]* ) 
-      # Save the password to a file, for example, $HOME/.passwords
-      echo "$password" >> ~/.passwords
-      echo "Password saved to ~/.passwords."
-      ;;
-    [Nn]* )
-      exit 0
-      ;;
-    * ) 
-      echo "Invalid input. Please enter 'y' or 'n'."
-      ;;
-  esac
+    # Print the password and ask if it should be saved
+    echo "Generated Password: $password"
+    read -r -p "Do you want to save this password? (y/n): " response
+    case $response in
+    [Yy]*)
+        # Ask if user wants to add a name/description
+        read -r -p "Add a name/description for this password (leave empty for none): " name
+        if [ -n "$name" ]; then
+            echo "$name: $password" >>~/.passwords
+        else
+            echo "$password" >>~/.passwords
+        fi
+        echo "Password saved to ~/.passwords."
+        ;;
+    [Nn]*)
+        exit 0
+        ;;
+    *)
+        echo "Invalid input. Please enter 'y' or 'n'."
+        ;;
+    esac
 done
